38140a7b1f5799152cf9bf2a73ca4454c6d143f8
zagizik <63964521+zagizik@users.noreply.github.com>
2020-07-19 14:37:54 +0300

    AR app

--- HoldTheMoon/HoldTheMoon/ViewController.swift
+++ HoldTheMoon/HoldTheMoon/ViewController.swift
@@ -19,15 +19,63 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+//         Create a new scene
+//        let moonScene = SCNScene(named: "art.scnassets/luna.scn")!
+//        if let moonNode = moonScene.rootNode.childNode(withName: "moon", recursively: true){
+//                moonNode.position = SCNVector3(0, 0, -0.1)
+//                sceneView.scene.rootNode.addChildNode(moonNode)
+//            }
+
+//         Set the scene to the view
+//        sceneView.scene = scene
+       
+//
+    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            let touchLocation = touch.location(in: sceneView)
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            if let hitResult = results.first{
+                let sphere = SCNSphere(radius: 0.1)
+                let material = SCNMaterial()
+                material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+                sphere.materials = [material]
+
+                 let node = SCNNode()
+                 node.position = SCNVector3(
+                    hitResult.worldTransform.columns.3.x,
+                    hitResult.worldTransform.columns.3.y + sphere.boundingSphere.radius,
+                    hitResult.worldTransform.columns.3.z)
+                 node.geometry = sphere
+                 sceneView.scene.rootNode.addChildNode(node)
+                 sceneView.autoenablesDefaultLighting = true
+
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor{
+            let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +83,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
