ab2915185dc4ce34ab119c9fb257e0e302fb1b3f
zagizik <63964521+zagizik@users.noreply.github.com>
2020-07-19 15:07:22 +0300

    Initial app commit

---
+++ Clima/Clima/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Александр Банников on 19.07.2020.
+//  Copyright © 2020 Александр Банников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
